6c22e5f934d89c82392a0d1744758cab7be90338
Sergey Ermolaev <weekendarti@yandex.ru>
2025-03-17 00:13:40 +0300

    Sprint9

--- src/managers/InMemoryHistoryManager.java
+++ src/managers/InMemoryHistoryManager.java
@@ -7,8 +7,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import static java.util.Objects.isNull;
-
 public class InMemoryHistoryManager implements HistoryManager {
 
     Map<Integer, Node<Task>> history = new HashMap<>();
